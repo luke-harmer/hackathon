098b3eb3fb1ab4ed2d20b691650847d28227dac5
x1u49jd <60787940+x1u49jd@users.noreply.github.com>
2020-11-21 22:06:11 +0100

    Add files via upload

--- Greenhouse Gas Emissions Calculator/src/Application.java
+++ Greenhouse Gas Emissions Calculator/src/Application.java
@@ -4,10 +4,11 @@ public class Application {
     public static void main(String[] args)throws IOException{
         
     //Variables declared and initialised
-    double carbonOxide = 1879.0;
-    double methane = 0.037;
-    double nitrogen = 0.033;
+    final double carbonOxide = 1879.0;
+    final double methane = 0.037;
+    final double nitrogen = 0.033;
     int i = 0;
+    int nGassBill;
     
     InputStreamReader isr = new InputStreamReader(System.in);
     BufferedReader br = new BufferedReader(isr);
@@ -19,11 +20,11 @@ public class Application {
         try{
             System.out.print("Enter the value of your gas bill in m3: ");
             String gasBill = br.readLine();
-            int nGassBill = Integer.parseInt(gasBill);
+            nGassBill = Integer.parseInt(gasBill);
             i = 1;
         }
         catch(IllegalArgumentException e){
-            i = 0;
+            continue;
     }
     
     //Calculations
